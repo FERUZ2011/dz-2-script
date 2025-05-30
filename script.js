b6b741ae3adec321c3596e5e143346c8efa19f2a
FERUZ2011 <feruzbeksihnazarov425@gmail.com>
2025-05-30 12:34:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const nameBlock = document.getElementById("name")
+
+let name1 = prompt("Say your name")
+
+nameBlock.innerText = name1
+
+
+
+const ageBlock = document.getElementById("age")
+
+let age1 = prompt("Say your age")
+
+ageBlock.innerText = age1
+
+
+
+const surnameBlock = document.getElementById("surname")
+
+let surname1 = prompt("Say your surname")
+
+surnameBlock.innerText = surname1
+
+
+
+const hometownBlock = document.getElementById("hometown")
+
+let hometown1 = prompt("Say your hometown")
+
+hometownBlock.innerText = hometown1
+
+
+
+const genderBlock = document.getElementById("gender")
+
+let gender1 = prompt("Say your gender")
+
+genderBlock.innerText = gender1
+
+
+
+const schoolBlock = document.getElementById("school")
+
+let school1 = prompt("Say your school")
+
+schoolBlock.innerText = school1
